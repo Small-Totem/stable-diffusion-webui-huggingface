51c3f4af924c34917de0604ace3f82f4ddd456fa
Small-Totem <Small-Totem@users.noreply.github.com>
2023-02-03 12:07:07 +0800

    1.10(支持切换下载目录(主要就是支持一下kaggle))

--- scripts/huggingface.py
+++ scripts/huggingface.py
@@ -5,7 +5,7 @@ from huggingface_hub import model_info, hf_hub_download
 from huggingface_hub.utils import RepositoryNotFoundError, RevisionNotFoundError
 from modules import scripts, script_callbacks 
 
-def download_model(_repo_id,_folder,_filename,_token):
+def download_model(_repo_id,_folder,_filename,_token,_cache_dir):
     try:
         repo_exists = True
         r_info = model_info(_repo_id, token=_token)
@@ -18,12 +18,13 @@ def download_model(_repo_id,_folder,_filename,_token):
         final_file_name=_filename
     else:    
         final_file_name=_folder+'/'+_filename
-    hf_hub_download(repo_id=_repo_id,token=_token,resume_download=True,cache_dir="/content/stable-diffusion-webui/models/Stable-diffusion/",
+    hf_hub_download(repo_id=_repo_id,token=_token,resume_download=True,cache_dir=_cache_dir,
                 filename=final_file_name,
                 force_filename=_filename,
                 )
-    print(_folder+'/'+_filename+" done.")            
-    return _folder+'/'+_filename+" done."
+    info_ret=_folder+'/'+_filename+" done. ->"+_cache_dir
+    print(info_ret)            
+    return info_ret
 
 
 def on_ui_tabs():     
@@ -33,9 +34,11 @@ def on_ui_tabs():
         ### Example
         download_model(repo_id,folder,filename,token)  
         repo_id=SmallTotem/reserved  
-        folder=Uncategorized (optional)  
+        folder=Uncategorized *(optional)*  
         filename=xxx.ckpt  
-        token=hf_xxx (optional)
+        token=hf_xxx *(optional)*  
+        target_dir=/content/stable-diffusion-webui/models/Stable-diffusion/ *(colab)*  
+                  =/kaggle/working/stable-diffusion-webui/models/Stable-diffusion/ *(kaggle)*  
         """)
         with gr.Group():
             with gr.Box():
@@ -43,10 +46,13 @@ def on_ui_tabs():
                     text_repo_id = gr.Textbox(show_label=False,value="SmallTotem/reserved", max_lines=1, placeholder="repo_id")
                     text_folder = gr.Textbox(show_label=False,value="Uncategorized", max_lines=1, placeholder="folder")
                     text_filename = gr.Textbox(show_label=False,value="", max_lines=1, placeholder="filename")
-                    text_token = gr.Textbox(show_label=False,value="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", max_lines=1, placeholder="🤗 token")
-                    out_file = gr.Textbox(show_label=False)
+                    text_token = gr.Textbox(show_label=False,value="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", max_lines=1, placeholder="🤗token")
+                with gr.Row().style(equal_height=True):
+                    text_target_dir = gr.Textbox(show_label=False,value="/content/stable-diffusion-webui/models/Stable-diffusion/", max_lines=1, placeholder="target_dir")
                 with gr.Row().style(equal_height=True):
                     btn_download = gr.Button("download")
-            btn_download.click(download_model, inputs=[text_repo_id, text_folder, text_filename, text_token], outputs=out_file)
+                with gr.Row().style(equal_height=True):
+                    out_file = gr.Textbox(show_label=False)
+            btn_download.click(download_model, inputs=[text_repo_id, text_folder, text_filename, text_token,text_target_dir], outputs=out_file)
     return (huggingface, "Hugging Face", "huggingface"),
 script_callbacks.on_ui_tabs(on_ui_tabs)
